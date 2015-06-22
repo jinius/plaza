a0f54cae40e0f5e6d40510692d029aa16e59a644
Lee JinWoo <jinus.kr@gmail.com>
2015-06-22 11:05:52 +0900

    api/register handler refactoring

--- handlers/user.js
+++ handlers/user.js
@@ -6,18 +6,12 @@ var UserManager = require('../models/user').UserManager;
 var userManager = new UserManager(db);
 
 exports.register = function(req, res) {
-	var body = req.body;
-	console.log("Register :", typeof body, body);
+	var body = req.body || {};
+	if (! body.userId) return res.status(400).json({ "error": "userId was not defined" });
 
-	if (! body.userId) {
-		return res.status(400).json({ "error": "userId was not defined" });
-	}
-
-	userManager.addUser( {
-		"userId" : body.userId
-	}, function(err, _id) {
+	var user = { "userId" : body.userId };
+	userManager.addUser(user, function(err, _id) {
 		if (err) return res.json({ "error": error.message });
-
 		res.json({ "error": null, "authKey": _id });
 	});
 };
