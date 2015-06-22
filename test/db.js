26f4020d103ed139a714e0e47768958a3ce08d2c
Lee JinWoo <jinus.kr@gmail.com>
2015-06-22 11:13:01 +0900

    DB manager test

--- test/db.js
+++ test/db.js
@@ -5,22 +5,28 @@ var should = require("should");
 var db = require("../models/db");
 
 describe("DB test", function() {
-	it("should connect successfully", function(done) {
+	it("connect test", function(done) {
+		db.master.connect(done);
+	});
+
+	it("collection test", function(done) {
+		db.master.collection("test", done);
+	});
+
+	it("connectionTest test", function(done) {
 		db.master.connectionTest(done);
 	});
 
-	it("insert data", function(done) {
-		db.master.collection("test", function(err, collection) {
-			if (err) return done(err);
-			collection.insertOne({"test" : "test"}, done);
-		});
+	it("insertOne", function(done) {
+		db.master.insertOne("test", {"test" : "test"}, done);
+	});
+
+	it("findOne", function(done) {
+		db.master.findOne("test", {"test" : "test"}, done);
 	});
 
 	it("drop collection", function(done) {
-		db.master.collection("test", function(err, collection) {
-			if (err) return done(err);
-			collection.drop(done);
-		});
+		db.master.dropCollection("test", done);
 	});
 });
 
