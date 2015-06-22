d91fa666400a47582871d656c6372e4db8b9767e
Lee JinWoo <jinus.kr@gmail.com>
2015-06-22 11:13:25 +0900

    User manager & test

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var ObjectID = require("mongodb").ObjectID;
+
 var UserManager = function(db) {
 	this.db = db.master;
 }
@@ -12,16 +14,41 @@ var UserManager = function(db) {
 //	callback = function(err, _id)
 // --------------------------------
 UserManager.prototype.addUser = function(user, callback) {
-	console.log("addUser: ", user);
 	if (! user.userId) return callback(new Error("userId not specified"));
 
 	this.db.collection("users", function(err, collection) {
 		if (err) return callback(err);
 
-		collection.insert( user, function(err, result) {
+		collection.insert(user, function(err, result) {
 			if (err) return callback(err);
 
 			callback(null, user._id);
+		});
+	});
+};
+
+UserManager.prototype.getUser = function(authKey, callback) {
+	if (! authKey) return callback(new Error("authKey not specified"));
+
+	this.db.collection("users", function(err, collection) {
+		if (err) return callback(err);
+
+		collection.findOne({
+			"_id" : ObjectID(authKey)
+		}, function(err, result) {
+			if (err) return callback(err);
+
+			callback(null, result);
+		});
+	});
+};
+
+UserManager.prototype.removeAllUser = function(callback) {
+	this.db.collection("users", function(err, collection) {
+		if (err) return callback(err);
+		collection.drop(function(err) {
+			if (err && err.message == "ns not found") return callback();
+			callback(err);
 		});
 	});
 };
---
+++ test/user.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var should = require("should");
+var ObjectID = require("mongodb").ObjectID;
+
+var db = require("../models/db");
+var UserManager = require("../models/user").UserManager;
+
+var userManager = new UserManager(db);
+
+describe("UserManager test", function() {
+	beforeEach(function(done) {
+		db.master.dropCollection("users", done);
+	});
+
+	it("add user", function(done) {
+		// User Manager : Add new user
+		var user = { "userId" : "add user test" };
+		userManager.addUser(user, function(err, _id) {
+			// DB : Find the user
+			if (err) return done(err);
+			var query = { "_id" : ObjectID(_id) };
+			db.master.findOne("users", query, function(err, result) {
+				// Compare _id
+				if (err) return done(err);
+				should.equal(result._id.toString(), _id.toString());
+				done();
+			});
+		});
+	});
+
+	it("get user", function(done) {
+		// DB : Add new user
+		var user1 = { "userId" : "get user test" };
+		db.master.insertOne("users", user1, function(err, result) {
+			// User Manager : Find the user
+			if (err) return done(err);
+			userManager.getUser(user1._id, function(err, user2) {
+				// Compare _id
+				if (err) return done(err);
+				should.equal(user1._id.toString(), user2._id.toString());
+				done();
+			});
+		});
+	});
+
+	it("remove all user", function(done) {
+		var user = { "userId" : "get user test" };
+		// DB : Add new user
+		db.master.insertOne("users", user, function(err, result) {
+			// User Manager : Remove all user
+			if (err) return done(err);
+			userManager.removeAllUser(function(err) {
+				// DB : Find the user
+				var query = { "_id" : ObjectID(user._id) };
+				db.master.findOne("users", query, function(err, result) {
+					should.not.exist(result);
+					done();
+				});
+			});
+		});
+	});
+
+	it("remove all user when empty", function(done) {
+		userManager.removeAllUser(done);
+	});
+});
+
