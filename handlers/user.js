5c37061c805fc5115363de2578886bf37a993067
Lee JinWoo <jinus.kr@gmail.com>
2015-06-22 11:32:52 +0900

    api/user handler : log message added

--- handlers/user.js
+++ handlers/user.js
@@ -7,6 +7,8 @@ var userManager = new UserManager(db);
 
 exports.register = function(req, res) {
 	var body = req.body || {};
+	console.log();
+	console.log("Register User :", typeof body, body);
 	if (! body.userId) return res.status(400).json({ "error": "userId was not defined" });
 
 	var user = { "userId" : body.userId };
